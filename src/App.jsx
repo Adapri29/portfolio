d23c3d23589fc9652aa060cc1635451377dbd074
Alberto Adamuz Priego <adapri29@gmail.com>
2024-11-08 21:05:36 +0100

    About me 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ function App() {
     <>
       
       <AboutMe />
+
+      <div className="skills">
+        <h1>Habilidades</h1>
+
+      </div>
     
     </>
   )
--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -1,3 +1,6 @@
+import "../styles/AboutMe.css"
+import photo from "../assets/photo.jpeg"
+
 const openPDF = () => {
     window.open('/public/Curriculum.pdf', '_blank');
 };
@@ -8,14 +11,17 @@ export const AboutMe = () => {
     return (
         <div className="about-me">
             <div className="info-container">
-            <img
-                src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/06/Things-Fate-Change-About-Gilgamesh-Mythology.jpg"
-                alt="Alberto Adamuz Priego"
-            />
-            <div>
+            <div className="info-container-left">
+                <img
+                    src={photo}
+                    alt="Alberto Adamuz Priego"
+                />
+                <button onClick={openPDF}>Descargar CV</button>
+            </div>
+            <div className="info-container-right">
                 <h1>Adapri</h1>
                 <h2>Alberto Adamuz Priego</h2>
-                <button onClick={openPDF}>Descargar CV</button>
+                <p>adapri29@gmail.com</p>
             </div>
             </div>
             <p>
@@ -24,6 +30,14 @@ export const AboutMe = () => {
                 Busco aprender, colaborar y crecer en cada proyecto, 
                 explorando oportunidades para aportar y evolucionar continuamente.
             </p>
+            <div className="contact">
+                <a href="https://github.com/Adapri29" target="_blank" rel="noopener noreferrer">
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/alberto-adamuz-priego-9b01b3276/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://www.freeiconspng.com/uploads/linkedin-icon-1.png" alt="linkedIn"></img>
+                </a>
+            </div>
         
         </div>
     )
